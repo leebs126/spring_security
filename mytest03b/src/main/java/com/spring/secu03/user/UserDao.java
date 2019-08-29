2e018535204d6d52acda19df062ada747726a8ad
leebs126 <leebs126@DESKTOP-URMM4P2>
2019-08-29 16:05:10 +0900

    최초 업로드

---
+++ mytest03b/src/main/java/com/spring/secu03/user/UserDao.java
@@ -0,0 +1,19 @@
+package com.spring.secu03.user;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+@Repository("userDao")
+public class UserDao {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public void insertUser(UserVO userVO) throws DataAccessException{
+		sqlSession.insert("mapper.user.insertUser", userVO);
+	}
+
+}
+
+
