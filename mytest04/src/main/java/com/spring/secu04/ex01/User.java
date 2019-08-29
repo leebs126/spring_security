3f4a053c9bc6aecc1a17e175cd71bb301393d901
leebs126 <leebs126@DESKTOP-URMM4P2>
2019-08-29 16:06:18 +0900

    최초 업로드

---
+++ mytest04/src/main/java/com/spring/secu04/ex01/User.java
@@ -0,0 +1,137 @@
+package com.spring.secu04.ex01;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
+	private String username;
+	private String password;
+	private boolean enabled;
+	boolean accountNonExpired;
+	boolean credentialsNonExpired;
+	boolean accountNonLocked;
+	private Set<GrantedAuthority> authorities;
+
+	public User(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+		this(username, password, true, true, true, true, authorities);
+	}
+
+	public User(String username, String password, boolean enabled, boolean accountNonExpired,
+			boolean credentialsNonExpired, boolean accountNonLocked,
+			Collection<? extends GrantedAuthority> authorities) {
+
+		if (((username == null) || "".equals(username)) || (password == null)) {
+			throw new IllegalArgumentException("Cannot pass null or empty values to constructor");
+		}
+
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.accountNonExpired = accountNonExpired;
+		this.credentialsNonExpired = credentialsNonExpired;
+		this.accountNonLocked = accountNonLocked;
+		this.authorities = Collections.unmodifiableSet(sortAuthorities(authorities));
+	}
+
+	public User(String username, String password, boolean enabled) {
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+	}
+
+	public User(String username, String password, boolean enabled, Set<GrantedAuthority> authorities) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.authorities = authorities;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	public void setAuthorities(Set<GrantedAuthority> authorities) {
+		this.authorities = authorities;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return this.accountNonExpired;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return this.accountNonLocked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return this.credentialsNonExpired;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return this.enabled;
+	}
+	
+	private static SortedSet<GrantedAuthority> sortAuthorities(Collection<? extends GrantedAuthority> authorities){
+		SortedSet<GrantedAuthority> sortedAuthorities = 
+				new TreeSet<GrantedAuthority>(new AuthorityComparator());
+		for(GrantedAuthority grantedAuthority : authorities) {
+			sortedAuthorities.add(grantedAuthority);
+		}
+		return sortedAuthorities;
+	}
+	
+	private static class AuthorityComparator implements Comparator<GrantedAuthority>, Serializable{
+		
+		public int compare(GrantedAuthority g1, GrantedAuthority g2) {
+			if(g2.getAuthority() == null) {
+				return -1;
+			}
+			
+			if(g1.getAuthority() == null) {
+				return 1;
+			}
+			return g1.getAuthority().compareTo(g2.getAuthority());
+			
+		}
+	}
+	
+
+}
